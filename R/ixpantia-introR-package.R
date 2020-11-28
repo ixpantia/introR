06b10631cefd0478fad7fae06741a40e23525020
FvD <FvD@users.noreply.github.com>
2020-11-27 18:23:51 -0600

    include package roxygen file

---
+++ R/ixpantia-introR-package.R
@@ -0,0 +1,8 @@
+#' Paquete en R para incluir ejercicios en RStudio
+#'
+#' El paquete ixpantia.introR da acceso a una serie de ejercicios dentro de la
+#' interfaz de RStudio.
+#' Ofreemos los siguientes ejercicios.
+#' @keywords internal
+"_PACKAGE"
+
