7e4daad4ceec87427ef75664b72474686f3c2d7e
andreavargasmontero <avargas2398@gmail.com>
2021-02-09 18:26:20 -0600

    Cambios para documentacion

--- R/lista_tutoriales.R
+++ R/lista_tutoriales.R
@@ -1,10 +1,10 @@
-#' Función para listar los tutoriales disponibles en el paquete
+#' Listar los tutoriales disponibles en el paquete
 #'
 #' @import learnr
 #' @return lista Lista de tutoriales
 lista_tutoriales <- function() {
 
-  tutoriales <- available_tutorials_for_package("ixpantia.introR")
+  tutoriales <- learnr::available_tutorials(package = "ixpantia.introR")
   lista <- tutoriales$tutorials
   return(lista)
 }
